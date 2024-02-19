7b031200012af62fa9dbb5d1669135624386ae47
unknown <squebler@gmail.com>
2024-02-18 22:25:47 -0700

    Added stopwatch.py and documentation.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# py-stopwatch
+# Description
+This is a stopwatch GUI app with start, stop, and reset functions.
+
+Hitting start while already started has no effect.
+
+When you hit stop, the time is not reset.
+
+You can hit start to resume from the last stop. For example, if you stop at
+1.5 seconds, and then wait 5 seconds, and then hit start, the time continues
+from 1.5 seconds; not 6.5 seconds.
+
+reset stops and resets the time to 0.
+
+stopping while already stopped has no effect.
+
+# Running without Console Window on Windows
+On Windows, you can run this without a console window via
+```
+> pythonw .\stopwatch.py 
+```
---
+++ stopwatch.py
@@ -0,0 +1,148 @@
+# stopwatch.py
+#
+# This is a stopwatch GUI app with start, stop, and reset functions.
+#
+# Hitting start while already started has no effect.
+#
+# When you hit stop, the time is not reset.
+#
+# You can hit start to resume from the last stop. For example, if you stop at
+# 1.5 seconds, and then wait 5 seconds, and then hit start, the time continues
+# from 1.5 seconds; not 6.5 seconds.
+#
+# reset stops and resets the time to 0.
+#
+# stopping while already stopped has no effect.
+#
+# On Windows, you can run this without a console window via
+# > pythonw .\stopwatch.py 
+#
+
+import os
+import sys
+import threading
+import time
+import tkinter as tk
+import traceback
+
+ONE_MS = 1/1000
+SEC_PER_MIN = 60
+SEC_PER_HR = SEC_PER_MIN * 60
+
+start = 0
+running = False
+reset = False
+ellapsedSinceStart = 0
+ellapsedBeforeStop = 0
+ellapsedTotal = 0
+updateThread = 0
+appRunning = True
+eventUpdateTime = "<<UpdateTime>>"
+strTime = ""
+
+
+def startClock():
+    global start, running
+    if not running:
+        start = time.time()
+    running = True
+
+def stopClock():
+    global running
+    running = False
+
+def resetClock():
+    global running, reset
+    running = False
+    reset = True
+
+def manageThread():
+    global reset, ellapsedSinceStart, ellapsedBeforeStop, ellapsedTotal, updateThread
+    oldVal = running
+    valChanged = False
+    while appRunning:
+        time.sleep(ONE_MS)
+        valChanged = oldVal != running
+        if valChanged:
+            if running:
+                updateThread = threading.Thread(target=updateClock)
+                updateThread.start()
+            else:
+                updateThread.join()
+                updateThread = 0
+                ellapsedBeforeStop += ellapsedSinceStart
+        if reset:
+            lbl_time["text"] = "00:00:00.000"
+            ellapsedBeforeStop = 0
+            ellapsedTotal = 0
+            reset = False
+        oldVal = running
+
+def updateClock():
+    global strTime, ellapsedTotal, ellapsedSinceStart
+    while running:
+        time.sleep(ONE_MS)
+        ellapsedSinceStart = time.time() - start
+        ellapsedTotal = ellapsedBeforeStop + ellapsedSinceStart
+        hours = divmod(ellapsedTotal, SEC_PER_HR)[0]
+        remaining = ellapsedTotal - (hours * SEC_PER_HR)
+        minutes = divmod(remaining, SEC_PER_MIN)[0]
+        seconds = remaining - (minutes * SEC_PER_MIN)
+        # print(f"{int(hours):02d}:{int(minutes):02d}:{seconds:06.3f}")
+        strTime = f"{int(hours):02d}:{int(minutes):02d}:{seconds:06.3f}"
+        window.event_generate(eventUpdateTime, when="tail")
+
+def on_updateTime(*args):
+    lbl_time["text"] = strTime
+
+def on_closing():
+    global running
+    running = False
+    if updateThread != 0:
+        window.after(1, on_closing)
+        return
+    window.destroy()
+
+try:
+    fontSize = 25
+
+    window = tk.Tk()
+    window.title("stopwatch")
+    window.resizable(False,False)
+    window.bind(eventUpdateTime, on_updateTime)
+    window.protocol("WM_DELETE_WINDOW", on_closing)
+
+    frame_clock = tk.Frame(master=window, bg="yellow")
+
+    lbl_time = tk.Label(master=frame_clock, text="00:00:00.000", font=("Arial",fontSize), width=14, height=2, bg="#FFFFFF")
+    lbl_time.pack()
+
+    frame_buttons = tk.Frame(master=window)
+    frame_buttons.rowconfigure(0, minsize=50)
+    frame_buttons.columnconfigure([0,1,2], minsize=50)
+
+    btn_start = tk.Button(master=frame_buttons, text="start", command=startClock, font=("Arial",fontSize), bg="#00DD00")
+    btn_start.grid(row=0, column=0)
+
+    btn_stop = tk.Button(master=frame_buttons, text="stop", command=stopClock, font=("Arial",fontSize), bg="red")
+    btn_stop.grid(row=0, column=1)
+
+    btn_reset = tk.Button(master=frame_buttons, text="reset", command=resetClock, font=("Arial",fontSize), bg="blue", fg="#EEEEFF")
+    btn_reset.grid(row=0, column=2)
+
+    frame_clock.pack()
+    frame_buttons.pack()
+
+
+    threadMgr = threading.Thread(target=manageThread)
+    threadMgr.start()
+
+    window.mainloop()
+
+    appRunning = False
+    threadMgr.join()
+
+except:
+    traceback.print_exc()
+    os._exit(1)
+
